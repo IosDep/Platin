64431f01fbb0a48392d70e0af5fefa85f582ebcd
Reham Abuhadba <rehamkhalil003@gmail.com>
2024-07-04 09:44:33 +0300

    Initial commit

---
+++ Platin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Platin
+//
+//  Created by Reham Khalil on 04/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
